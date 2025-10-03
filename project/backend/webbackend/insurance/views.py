af7cbffca61dcf00d2557473ab1199bb651248f2
Geetik-Sharma <sharmageetik@gmail.com>
2025-10-03 11:09:10 +0530

    complete product class view for get post put and delete items

--- project/backend/webbackend/insurance/views.py
+++ project/backend/webbackend/insurance/views.py
@@ -1,7 +1,1 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-def home(request):
-    #normal response
-    stuinfo=["name","geetik","age",21,"dob",181104]
-    return HttpResponse( stuinfo )
